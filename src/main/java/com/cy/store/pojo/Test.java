5c247715d269edc7b3a5d7c610c0df9660c7c0b6
laomeng <2052209320@qq.com>
2022-08-27 23:02:21 +0800

    firstCommit_Test_001

--- src/main/java/com/cy/store/pojo/Test.java
+++ src/main/java/com/cy/store/pojo/Test.java
@@ -3,5 +3,6 @@ package com.cy.store.pojo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("lalalalalala");
+        System.out.println("第三次提交");
     }
 }
