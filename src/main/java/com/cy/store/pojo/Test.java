daf643d4b4e3dbc723dfdf1aeab524087e1d0173
laomeng <2052209320@qq.com>
2022-08-27 23:08:51 +0800

    热修分支修改了

--- src/main/java/com/cy/store/pojo/Test.java
+++ src/main/java/com/cy/store/pojo/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("lalalalalala");
         System.out.println("第三次提交");
+        System.out.println("热修分支修改了");
     }
 }
