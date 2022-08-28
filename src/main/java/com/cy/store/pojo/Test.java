b315fbc4aa0b32b530354365e8b6918dcdae4ca6
laomeng <2052209320@qq.com>
2022-08-28 10:06:27 +0800

    github更改

--- src/main/java/com/cy/store/pojo/Test.java
+++ src/main/java/com/cy/store/pojo/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("lalalalalala");
         System.out.println("第三次提交");
         System.out.println("master修改了");
+        System.out.println("github");
     }
 }
