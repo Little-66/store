c82407498a368cc9c14c667d738aee20d6b7bf35
laomeng <2052209320@qq.com>
2022-08-28 11:09:41 +0800

    store1.0_NoCache

--- src/main/java/com/cy/store/pojo/Test.java
+++ src/main/java/com/cy/store/pojo/Test.java
@@ -2,11 +2,6 @@ package com.cy.store.pojo;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("lalalalalala");
-        System.out.println("第三次提交");
-        System.out.println("master修改了");
-        System.out.println("github");
-        System.out.println("github");
-        
+
     }
 }
