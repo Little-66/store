0121c62bb6741aa602d659c0318f01a2232372e5
Little-66 <99248707+Little-66@users.noreply.github.com>
2022-08-28 10:15:09 +0800

    Update Test.java

--- src/main/java/com/cy/store/pojo/Test.java
+++ src/main/java/com/cy/store/pojo/Test.java
@@ -6,5 +6,7 @@ public class Test {
         System.out.println("第三次提交");
         System.out.println("master修改了");
         System.out.println("github");
+        System.out.println("github");
+        
     }
 }
